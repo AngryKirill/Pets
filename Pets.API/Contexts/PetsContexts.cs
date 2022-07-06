47ace92c64cffde67f6394bfd823a412f72d10be
AngryKirill <mister.lolich@mail.ru>
2022-07-06 10:18:26 +0300

    Visual 2022

---
+++ Pets.API/Contexts/PetsContexts.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pets.API.Entities;
+
+namespace Pets.API.Contexts
+{
+    public class PetsContexts : DbContext
+    {
+        public DbSet<Pet> Pets { get; set; }
+
+
+    }
+}
