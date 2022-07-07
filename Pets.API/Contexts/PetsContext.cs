b54f8167f8d0f12041b332f640efe747d45adc2b
ArtyomBanchikov <87804247+AngryKirill@users.noreply.github.com>
2022-07-07 10:08:56 +0300

    Visual 2022 (#4)
    * Visual 2022
    
    * Add CRUD controller
    
    * Fixes
    
    * Update change
    
    * rename and fix updating
    
    * Rename

---
+++ Pets.API/Contexts/PetsContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pets.API.Entities;
+
+namespace Pets.API.Contexts
+{
+    public class PetsContext : DbContext
+    {
+        public DbSet<Pet> Pets { get; set; }
+
+        public PetsContext(DbContextOptions<PetsContext> options)
+        : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pet>().HasData(
+                    new Pet { Id = 1, Name = "Tom", Age = 37, Weight = 10 },
+                    new Pet { Id = 2, Name = "Bob", Age = 41, Weight = 10 },
+                    new Pet { Id = 3, Name = "Sam", Age = 24, Weight = 11 }
+            );
+        }
+    }
+}
---
+++ Pets.API/Controllers/PetController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pets.API.Contexts;
+using Pets.API.Entities;
+
+namespace Pets.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetController : ControllerBase
+    {
+        private readonly PetsContext _context;
+
+        public PetController(PetsContext context)
+        {
+            _context = context; 
+        }
+
+        [HttpGet]
+        public IEnumerable<Pet> GetAll()
+        {
+            return _context.Pets.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Pet GetById(int id)
+        {
+            var pet = _context.Pets.Find(id);
+            if (pet == null)
+                throw new ArgumentNullException("Object cannot be null", nameof(pet));
+            return pet;
+        }
+
+        [HttpPost]
+        public void Add(Pet pet)
+        {
+            _context.Pets.Add(pet);
+            _context.SaveChanges();
+        }
+
+        [HttpPut]
+        public void Update(Pet pet)
+        {
+            _context.Pets.Update(pet);
+            _context.SaveChanges();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var dbPet = _context.Pets.Find(id);
+            if(dbPet == null)
+                throw new ArgumentNullException("Object cannot be null", nameof(dbPet));
+
+            _context.Pets.Remove(dbPet);
+            _context.SaveChanges();
+        }
+    }
+}
