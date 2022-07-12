9db4063900fc06d0cd9d2a231c5a8cbb9e67f6e8
ArtyomBanchikov <87804247+AngryKirill@users.noreply.github.com>
2022-07-12 13:43:03 +0300

    Adding DI and AutoMapper profiles (#6)
    add AutoMapper profiles, Di settings and using interfaces

---
+++ Pets.BLL/Mappers/ModelProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Pets.BLL.Models;
+using Pets.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets.BLL.Mappers
+{
+    public class ModelProfile: Profile
+    {
+        public ModelProfile()
+        {
+            CreateMap<Pet, PetEntity>().ReverseMap();
+        }
+    }
+}
