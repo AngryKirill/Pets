86650eff9f479f0925cb40e41d0738a801c9c581
ArtyomBanchikov <87804247+AngryKirill@users.noreply.github.com>
2022-07-15 10:20:37 +0300

    New entity migration (#8)

--- Pets.DAL/Contexts/PetsContext.cs
+++ Pets.DAL/Contexts/PetsContext.cs
@@ -6,11 +6,12 @@ namespace Pets.DAL.Contexts
     public class PetsContext : DbContext
     {
         public DbSet<PetEntity> Pets { get; set; }
+        public DbSet<FoodEntity> Foods { get; set; }
 
         public PetsContext(DbContextOptions<PetsContext> options)
         : base(options)
         {
-            Database.EnsureCreated();
+            
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -19,6 +20,9 @@ namespace Pets.DAL.Contexts
                     new PetEntity { Id = 1, Name = "Tom", Age = 37, Weight = 10 },
                     new PetEntity { Id = 2, Name = "Bob", Age = 41, Weight = 10 },
                     new PetEntity { Id = 3, Name = "Sam", Age = 24, Weight = 11 }
+            );
+            modelBuilder.Entity<FoodEntity>().HasData(
+                    new FoodEntity { Id = 1, Calories = 10, Description = "For fat", Name = "Food1", ProducingCountry = "USA" }
             );
         }
     }
