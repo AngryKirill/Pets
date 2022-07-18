4c58d892326c2d11d037a285306c17f6d8675751
ArtyomBanchikov <87804247+AngryKirill@users.noreply.github.com>
2022-07-18 13:32:11 +0300

    Add generic service (#10)
    * Adding generic service and deleting pet service
    
    * Add DI lifetime, food model and food Model-Entity map

---
+++ Pets.BLL/Interfaces/IGenericService.cs
@@ -0,0 +1,14 @@
+﻿namespace Pets.BLL.Interfaces
+{
+    public interface IGenericService<TModel, TEntity> 
+        where TModel : class
+        where TEntity : class
+    {
+        IEnumerable<TModel> GetAll();
+        TModel GetById(int id);
+        IEnumerable<TModel> Find(Func<TModel, Boolean> predicate);
+        void Create(TModel item);
+        void Update(TModel item);
+        void Delete(int id);
+    }
+}
